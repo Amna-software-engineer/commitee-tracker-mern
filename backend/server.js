92e479c1ba2a454bb703de073379791e571db647
Amna-software-engineer <amnahaqamna83@gmail.com>
2026-07-01 13:46:39 +0500

    cors err

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors({ origin: process.env.CLIENT_ORIGIN || "https://commitee-tracker-mern.vercel.app/" , methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+app.use(cors({ origin: process.env.CLIENT_ORIGIN || "https://commitee-tracker-mern.vercel.app" , methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
     credentials: true }));
 app.use(express.json());
 
